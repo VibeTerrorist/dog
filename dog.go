e47e2f699337ff3910d2738eb59764802c757cd7
Vibe Terrorist <idart15.06@gmail.com>
2024-04-03 14:28:30 +0100

    Pushing some code for the lesson

---
+++ dog.go
@@ -0,0 +1,7 @@
+import (
+	"strings"
+)
+
+func WhenGrownUp(s string) string {
+	return "When the puppy grows it says: " + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/VibeTerrorist/dog
+
+go 1.22.1
